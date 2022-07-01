aaead90743ee49cc1a11806ca14a44982bbd362f
dfitriana <diyahayufa@students.unnes.ac.id>
2022-07-01 20:35:03 +0700

    Update index.jsx

--- pages/seller/index.jsx
+++ pages/seller/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 import { Container, Card, Button, Form, Nav, Navbar } from "react-bootstrap";
